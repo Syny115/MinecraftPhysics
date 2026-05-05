77b6abef5bce8d5acf122c4b3aa1a59403d5de06
Syny115 <Fernandogs1803@gmail.com>
2026-05-05 16:05:10 +0200

    tiled created

---
+++ Tiled/CastlevaniaTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="CastlevaniaTileset" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Documents/GitHub/MinecraftPhysics/Castlevania/Project1/resources/sprites/CastlevaniaTileset.png" width="512" height="512"/>
+</tileset>
